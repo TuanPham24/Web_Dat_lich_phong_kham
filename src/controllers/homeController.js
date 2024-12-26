e3e99f326ad42ded8bf3d7e87098a393d9bca492
TuanKoTinhTao <tuandangdihoc@gmail.com>
2024-12-27 01:44:38 +0700

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,7 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+  let data = await db.User.findAll();
+  return res.render("homepage.ejs", { data: JSON.stringify(data) });
 };
 
 module.exports = {
