8b2b2ea9c19048a9d929e6560d883dc61b17ed1c
TuanKoTinhTao <tuandangdihoc@gmail.com>
2025-06-10 14:07:01 +0700

    update code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,22 @@
-let handleLogin = (req, res) => {
+import userService from "../services/userService";
+let handleLogin = async (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
   if (!email || !password) {
     return res.status(500).json({
       errCode: 1,
-      message: "Email and password are required",
+      message: "Missing inputs parameter",
     });
   }
+
+  let userData = await userService.handleUserLogin(email, password);
+
   return res.status(200).json({
-    errCode: 0,
-    message: "hello world",
-    yourEmail: email,
-    test: "test",
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
   });
 };
 
-module.exports = { handleLogin };
+module.exports = { handleLogin: handleLogin };
