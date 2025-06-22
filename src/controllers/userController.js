2568f4a1020b3540358f29e731754654bf013039
TuanKoTinhTao <tuandangdihoc@gmail.com>
2025-06-22 22:47:36 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -19,4 +19,25 @@ let handleLogin = async (req, res) => {
   });
 };
 
-module.exports = { handleLogin: handleLogin };
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id;
+  if(!id){
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+      users: []
+    });
+  }
+  let users = await userService.getAllUsers(id);
+  // console.log(users);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users: users,
+  });
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
+};
